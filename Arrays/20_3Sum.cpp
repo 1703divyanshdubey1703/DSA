9bf913668d17d49e374c455b7aee04adda14339e
Divyansh Dubey <1703divyanshdubey1703@gmail.com>
2026-08-15 12:08:21 +0530

    Leet Code Problem 15

---
+++ Arrays/20_3Sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<vector<int>> ans;
+        int i=0;
+        int sum = 0;
+        for(int i=0;i<n;i++)
+        {
+            int left=i+1, right = n-1;;
+            if(i>0 && nums[i]==nums[i-1])
+                continue;
+            while(left<right)
+            {
+                sum=(nums[i]+nums[left]+nums[right]);
+                if(sum<0)
+                left++;
+                else if(sum>0)
+                right--;
+                else
+                {
+                    ans.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+                }
+            }
+        }
+        return ans;
+    }
+};
