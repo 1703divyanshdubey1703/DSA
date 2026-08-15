476d41da22e810135e370ae49bd87da1fd3eaaf6
Divyansh Dubey <1703divyanshdubey1703@gmail.com>
2026-08-15 12:21:26 +0530

    Leet Code Problem 15

--- Arrays/20_3Sum.cpp
+++ Arrays/20_3Sum.cpp
@@ -23,7 +23,18 @@ public:
                     ans.push_back({nums[i], nums[left], nums[right]});
                     left++;
                     right--;
+                                    while(left<right && nums[left]==nums[left-1])
+                {
+                    left++;
                 }
+                while(left<right && nums[right]==nums[right+1])
+                {
+                    right--;
+                }
+                    
+                }
+                
+
             }
         }
         return ans;
